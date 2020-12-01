45fea024a07935ec56d1cc2912619d97d7e548da
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-01 15:51:34 +0000

    initial commit

---
+++ src/test/java/com/codeclan/example/FilesAndFolders/FilesAndFoldersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.FilesAndFolders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesAndFoldersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
